6625e32d12dba9f7ffb8fad2e5c5ece306aa56fd
Zeid Kootbally <zeidk@umd.edu>
2025-09-01 20:45:35 -0400

    modified file names

---
+++ week1/src/week1.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file week1.cpp
+ * @author Zeid Kootbally (zeidk@umd.edu)
+ * @brief Basic hello world example
+ * @version 0.1
+ * @date 2025-09-01
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include <iostream>
+
+/**
+ * @brief Main function
+ * 
+ * @return int Return status
+ */
+int main() {
+  std::cout << "Hello, from enpm702_fall2025!\n";
+}
---
+++ week2/src/week2.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello, from enpm702_fall2025!\n";
+}
